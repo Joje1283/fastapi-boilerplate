d29013b4d662c39dd55fe07a9cb8d10682487312
Joje1283 <joje1283@gmail.com>
2024-12-10 22:03:02 +0900

    Add test for login service

--- app/user/application/user_service_test.py
+++ app/user/application/user_service_test.py
@@ -1,8 +1,11 @@
+from fastapi import HTTPException
 from freezegun import freeze_time
 import pytest
+from datetime import datetime
 from ulid import ULID
 
-from app.user.application.schema.user import RegisterUserCommand
+from app.auth.application.auth_service import AuthService
+from app.user.application.schema.user import RegisterUserCommand, LoginQuery
 from app.user.application.user_service import UserCommandService, UserQueryService
 from app.user.domain.user import User, Profile
 from core.uow.abstract import AbcUnitOfWork
@@ -35,7 +38,7 @@ async def user_service_dependencies(mocker):
 
 @freeze_time("2024-12-08")
 @pytest.mark.asyncio
-async def test_create_user_success(user_service_dependencies):
+async def test_register_success(user_service_dependencies):
     (
         uow_mock,
         ulid_mock,
@@ -75,3 +78,84 @@ async def test_create_user_success(user_service_dependencies):
         )
     )
     user_command_service.crypto.encrypt.assert_called_once_with(password)
+
+
+@pytest.fixture
+async def user_query_service_dependencies(mocker):
+    uow_mock = mocker.Mock(spec=AbcUnitOfWork)
+    crypto_mock = mocker.Mock(spec=Crypto)
+    auth_service_mock = mocker.Mock(spec=AuthService)
+    uow_mock.user_repo = mocker.AsyncMock()
+
+    # async context manager (UnitOfWork)
+    async def aenter():
+        return uow_mock
+
+    async def aexit(exc_type, exc_val, exc_tb):
+        pass
+
+    uow_mock.__aenter__ = mocker.AsyncMock(side_effect=aenter)
+    uow_mock.__aexit__ = mocker.AsyncMock(side_effect=aexit)
+
+    return (
+        uow_mock,
+        auth_service_mock,
+        crypto_mock,
+    )
+
+
+@freeze_time("2024-12-08")
+@pytest.mark.asyncio
+async def test_login_success(user_query_service_dependencies):
+    (
+        uow_mock,
+        auth_service_mock,
+        crypto_mock,
+    ) = await user_query_service_dependencies
+    email = "test@test.com"
+    now = datetime.now()
+    uow_mock.user_repo.find_by_email.return_value = User(
+        id="01F9Z3KZ1E4QZJQZJZJZJZJZJZJ",
+        email=email,
+        profile=Profile(
+            name="test",
+            age=20,
+            phone="1234567890",
+        ),
+        password="password",
+        created_at=now,
+        updated_at=now,
+    )
+
+    user_query_service = UserQueryService(crypto=crypto_mock, read_uow=uow_mock, auth_service=auth_service_mock)
+    await user_query_service.login(
+        login_query=LoginQuery(
+            email=email,
+            password="password",
+        )
+    )
+    user_query_service.uow.user_repo.find_by_email.assert_called_once_with(email)
+    user_query_service.crypto.verify.assert_called_once_with("password", "password")
+
+
+@freeze_time("2024-12-08")
+@pytest.mark.asyncio
+async def test_login_fail(user_query_service_dependencies):
+    (
+        uow_mock,
+        auth_service_mock,
+        crypto_mock,
+    ) = await user_query_service_dependencies
+    email = "test@test.com"
+    now = datetime.now()
+    uow_mock.user_repo.find_by_email.return_value = None
+
+    user_query_service = UserQueryService(crypto=crypto_mock, read_uow=uow_mock, auth_service=auth_service_mock)
+    with pytest.raises(HTTPException):
+        await user_query_service.login(
+            login_query=LoginQuery(
+                email=email,
+                password="password",
+            )
+        )
+    user_query_service.uow.user_repo.find_by_email.assert_called_once_with(email)
