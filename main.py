8612d57ada3e4c326da8ddabb76f8c076d5dbe1d
Joje1283 <joje1283@gmail.com>
2025-01-07 21:49:44 +0900

    Add Lifespan function for management the redis client.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from core.infra.logger import logger
 from core.infra.containers import Container
+from core.infra.redis import redis_client
 from core.interface.controller.user_controller import router as user_routers
 from core.interface.controller.post_controller import router as post_routers
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        yield
+    finally:
+        logger.info("Shutting down...")
+        await redis_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.container = Container()
 
 app.include_router(user_routers)
